d07ffb9abe9818842ea58a9908270a93f9ed7baa
Yarlen <2020030817@upsin.edu.mx>
2025-06-24 17:11:47 -0700

    1-DOM cambiar texto

---
+++ 1-dom/JS.js
@@ -0,0 +1,5 @@
+function cambiarTexto() {
+  let parrafo = document.getElementById("miParrafo");
+  parrafo.textContent = "texto cambiado"; // deberia de cambiar el contendido del texto pero no funciona
+  parrafo.style.color = "red";
+}
